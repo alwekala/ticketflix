cdbe192a1a59bfeb4648e8a0a3ae9b43332592bc
projektbot <projektbot@protonmail.com>
2022-07-31 15:05:10 +0200

    small change

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -10,7 +10,7 @@ import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p> Update3
+      <p> Update 4
         : I'm making this by following the Gatsby Tutorial!</p>
     </Layout>
   )
